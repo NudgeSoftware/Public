30578830f49afe8f32f8ccbc55b0fcf8521bd66d
Andrew <awrobins@gmail.com>
2016-10-14 14:01:51 -0400

    cleanup bootstrap-machine

--- bootstrap-machine.ps1
+++ bootstrap-machine.ps1
@@ -1,9 +1,19 @@
 ﻿# variables
-$nudgeDir = "C:\ProgramData\Nudge"
-$setupDir = "$nudgeDir\machine-setup"
-$codeDir = "C:\Code"
-$userDir = "C:\Users\$env:UserName"
-$logFile = "$env:LocalAppData\Boxstarter\Boxstarter.log"
+$user = Get-CimInstance Win32_UserAccount | Where-Object { $_.Caption -eq $(whoami) }
+$ps = New-Object PSObject -Property @{
+    NudgeDir = "C:\ProgramData\Nudge"
+    SetupDir = "C:\ProgramData\Nudge\machine-setup"
+    CodeDir = "C:\Code"
+    UserDir = "C:\Users\$env:UserName"
+    SshDir = "C:\Users\$env:UserName\.ssh"
+    EmailAddressFile = "C:\ProgramData\Nudge\machine-setup\emailaddress.txt"
+    LogFile = "$env:LocalAppData\Boxstarter\Boxstarter.log"
+}
+$bash = New-Object PSObject
+foreach ($p in Get-Member -InputObject $ps -MemberType NoteProperty) {
+     Add-Member -InputObject $bash -MemberType NoteProperty -Name $p.Name -Value $From.$($p.Name) –Force
+     $bash.$($p.Name) = $($ps.$($p.Name) -replace "\\", "/" -replace "C:", "/c")
+}
 
 # Prerequisite Validation
 $os = Get-CimInstance Win32_OperatingSystem
@@ -12,17 +22,25 @@ $prerequisites = New-Object PSObject -Property @{
     OSHasArchitecture = $os.OSArchitecture -eq '64-bit'
     OSHasType = ($os.Caption -like '*Pro') -or ($os.Caption -like '*Enterprise')
     UserNameValid = $env:UserName -notlike '* *'
-    UserMicrosoftAccount = $true
 }
-if (!$prerequisites.OSHasVersion -or !$prerequisites.OSHasArchitecture -or !$prerequisites.OSHasType -or !$prerequisites.UserNameValid -or !$prerequisites.UserMicrosoftAccount) {
+if (!$prerequisites.OSHasVersion -or !$prerequisites.OSHasArchitecture -or !$prerequisites.OSHasType -or !$prerequisites.UserNameValid) {
     if (!$prerequisites.OSHasVersion -or !$prerequisites.OSHasArchitecture -or !$prerequisites.OSHasType) { $host.ui.WriteErrorLine("Minimum supported version of Windows is Windows 10 Pro, 64-bit. You are running $($os.Caption), $($os.OSArchitecture)") }
     if (!$prerequisites.UserNameValid) { $host.ui.WriteErrorLine("UserName ($env:UserName) must not contain spaces: Modify in Start -> User Accounts before continuing") }
-    if (!$prerequisites.UserMicrosoftAccount) { $host.ui.WriteErrorLine("User account must be linked to Microsoft account (see https://support.microsoft.com/en-us/help/17201/windows-10-sign-in-with-a-microsoft-account)") }
+#    if (!$prerequisites.UserMicrosoftAccount) { $host.ui.WriteErrorLine("User account must be linked to Microsoft account (see https://support.microsoft.com/en-us/help/17201/windows-10-sign-in-with-a-microsoft-account)") }
     Exit
 }
 
-$lockFile = "$setupDir\bootstrap-machine.lock"
+if (!(Test-Path $ps.EmailAddressFile)) {
+    $emailAddress = Read-Host "What email do you use with git? "  
+    Add-Content -Path $ps.EmailAddressFile -Value "$emailAddress"
+} else {
+    $emailAddress = Get-Content -Path $ps.EmailAddressFile
+}
+
+# initial settings for windows & boxstarter
+$lockFile = "$($ps.SetupDir)\bootstrap-machine.lock"
 if (!(Test-Path $lockFile -NewerThan (Get-Date).AddHours(-2))) {
+    if (Test-PendingReboot) { Invoke-Reboot }
     try {
         # chocolatey initial setup
         choco feature enable -n=allowGlobalConfirmation -y
@@ -33,121 +51,103 @@ if (!(Test-Path $lockFile -NewerThan (Get-Date).AddHours(-2))) {
         Disable-InternetExplorerESC
         Update-ExecutionPolicy
         Update-ExecutionPolicy Unrestricted
-        Install-WindowsUpdate  -AcceptEula
-        if (Test-PendingReboot) { Invoke-Reboot }
-
-        Enable-MicrosoftUpdate
-        Enable-UAC
-
     } catch {
-        $logFile
+        Invoke-Item $ps.LogFile
+        throw
     }
 }
 New-Item $lockFile -Force
 
-if (!(Test-Path $setupDir)) {
-    New-Item -Path $setupDir -type Directory
-}
+# git install & setup
+$lockFile = "$($ps.SetupDir)\install-git.lock"
+if (!(Test-Path $lockFile -NewerThan (Get-Date).AddHours(-2))) {
+    if (Test-PendingReboot) { Invoke-Reboot }
+    try {
+        cinst dotnet3.5 -y
+        cinst lessmsi -y
+        cinst webpicmd -y
+        if (Test-PendingReboot) { Invoke-Reboot }
 
+        # hack to get lessmsi available
+        $env:Path += ";C:\ProgramData\chocolatey\bin"
 
-$settingsPath = "$nudgeDir\settings.ini"
-$settings = @{}
-if (Test-Path $settingsPath) {
-    Get-Content $settingsPath | foreach-object -begin {$settings=@{}} -process { $k = [regex]::split($_,'='); if(($k[0].CompareTo("") -ne 0) -and ($k[0].StartsWith("[") -ne $True)) { $settings.Add($k[0], $k[1]) } }
-}
+        # install Git
+        cinst git -y -params '"/GitAndUnixToolsOnPath /NoAutoCrlf"'
+        cinst poshgit -y
+        cinst gitextensions -y
+        if (Test-PendingReboot) { Invoke-Reboot }
 
-Write-Host ">> Configure settings"
-if (!($settings["email"])) {  
-    $settings["email"] = Read-Host "What email do you use with git? "  
-} 
+        # Configure git
+        $registryPath = "HKCU:\Software\GitExtensions"
+        if (!(Test-Path $registryPath)) {
+            New-Item -Path $registryPath -Force | Out-Null
+        }
+        New-ItemProperty -Path $registryPath -Name "gitssh" -Value "" -PropertyType String -Force | Out-Null
+        New-ItemProperty -Path $registryPath -Name "gitcommand" -Value "" -PropertyType String -Force | Out-Null
 
-if (!($settings["username"])) {
-    $settings["username"] = Read-Host "Input your Computer username (ENTER for $env:UserName) "
-    if (!($settings["username"])) {
-        $settings["username"] = $env:UserName
+        git config --global user.name $user.FullName
+        git config --global user.email $emailAddress
+        git config --global core.autocrlf False
+
+        # git ssh setup
+        if (!(Test-Path "$($ps.SshDir)\id_rsa")) {
+            ssh-keygen -q -C $emailAddress -f $bash.SshDir/id_rsa
+        }
+
+        ssh-agent -s
+        ssh-add $bash.SshDir/id_rsa
+        Get-Content "$($bash.SshDir)\id_rsa.pub" | clip
+        Invoke-Item "$($bash.SshDir)\id_rsa.pub"
+        Start-Process -FilePath "https://github.com/settings/ssh"
+
+        Write-Host "Copied the full contents of $($bash.SshDir)\id_rsa.pub (currently in your clipboard):"
+        Read-Host "Go to https://github.com/settings/ssh and add as a new key, then press ENTER"
+        ssh -T git@github.com
+    } catch {
+        Invoke-Item $ps.LogFile
+        throw
     }
 }
-if (!($settings["name"])) {
-    $settings["name"] = Read-Host "Input your Nudge name (ideally $($settings["username"]), but if that is not unique at Nudge then a unique value for all nudge dev resources -- ENTER for $($settings["username"])) "
-    if (!($settings["name"])) {
-        $settings["name"] = $settings["username"]
+New-Item $lockFile -Force
+
+$lockFile = "$($ps.SetupDir)\windows-update.lock"
+if (!(Test-Path $lockFile -NewerThan (Get-Date).AddHours(-2))) {
+    if (Test-PendingReboot) { Invoke-Reboot }
+    try {
+        Install-WindowsUpdate  -AcceptEula
+        if (Test-PendingReboot) { Invoke-Reboot }
+
+        Enable-MicrosoftUpdate
+        cinst boxstarter -y
+    } catch {
+        Invoke-Item $ps.LogFile
+        throw
     }
 }
+New-Item $lockFile -Force
 
-if (Test-Path $settingsPath) {
-    Remove-Item $settingsPath
-}
-foreach ($i in $settings.keys) {
-    Add-Content -Path $settingsPath -Value "$i=$($settings[$i])"
-}
 
-$profile = "C:\Users\$($settings['username'])"
-$bashProfile = "/c/Users/$($settings['username'])"
-
-# hack to get lessmsi available
-$env:Path += ";C:\ProgramData\chocolatey\bin"
-
-cinst dotnet3.5 -y
-cinst lessmsi -y
-cinst webpicmd -y
-
-# Windows Updates (this takes time)
-Install-WindowsUpdate -AcceptEula
-
-# install Git & associated
-cinst git -y -params '"/GitAndUnixToolsOnPath /NoAutoCrlf"'
-cinst gitextensions -y
-cinst poshgit -y
-cinst P4Merge -y
-
-# Configure git
-$registryPath = "HKCU:\Software\GitExtensions"
-if (!(Test-Path $registryPath)) {
-    New-Item -Path $registryPath -Force | Out-Null
-}
-New-ItemProperty -Path $registryPath -Name "gitssh" -Value "" -PropertyType String -Force | Out-Null
-New-ItemProperty -Path $registryPath -Name "gitcommand" -Value "" -PropertyType String -Force | Out-Null
-
-git config --global user.name $settings["name"]
-git config --global user.email $settings["email"]
-git config --global core.autocrlf False
-git config --global merge.tool p4merge
-git config --global diff.guitool p4merge
-git config --global difftool.p4merge.path "C:/Program Files/Perforce/p4merge.exe"
-git config --global difftool.p4merge.cmd '\"C:/Program Files/Perforce/p4merge.exe\" \"$REMOTE\" \"$MERGED\"'
-git config --global mergetool.p4merge.path "C:/Program Files/Perforce/p4merge.exe"
-git config --global mergetool.p4merge.cmd '\"C:/Program Files/Perforce/p4merge.exe\" \"$BASE\" \"$LOCAL\" \"$REMOTE\" \"$MERGED\"'
-
-if (!(Test-Path "$profile\.ssh")) {
-    cd $profile
-    mkdir .ssh
+if (Test-PendingReboot) { Invoke-Reboot }
+if (!(Test-Path $ps.SetupDir)) {
+    New-Item -Path $ps.SetupDir -type Directory
 }
 
 cinst boxstarter -y
 
-$codeDir = "C:\Code"
-if (!(Test-Path $codeDir)) {
-    New-Item -Path $codeDir -type Directory
+if (!(Test-Path $ps.CodeDir)) {
+    New-Item -Path $ps.CodeDir -type Directory
 }
-cd $codeDir
 
-if (!(Test-Path "$codeDir\Tooling")) {
-    # git ssh setup:
-    if (!(Test-Path "$profile\.ssh\id_rsa")) {
-        ssh-keygen -q -C $settings["email"] -f $bashProfile/.ssh/id_rsa
+if (!(Test-Path "$($ps.CodeDir)\Tooling")) {
+    if (Test-PendingReboot) { Invoke-Reboot }
+    try {
+        cd $ps.CodeDir
+        git clone git@github.com:NudgeSoftware/Tooling.git
+        cp "$($ps.CodeDir)\Tooling\*" $ps.SetupDir
+    } catch {
+        Invoke-Item $ps.LogFile
+        throw
     }
-
-    ssh-agent -s
-    ssh-add $bashProfile/.ssh/id_rsa
-    Get-Content "$profile\.ssh\id_rsa.pub" | clip
-    Start-Process -FilePath "https://github.com/settings/ssh"
-
-    Write-Host "Copied the full contents of $profile\.ssh\id_rsa (currently in your clipboard):"
-    Read-Host "Go to https://github.com/settings/ssh and add as a new key, then press ENTER"
-    ssh -T git@github.com
-
-    # clone repo
-    git clone git@github.com:NudgeSoftware/Tooling.git
 }
 
-bash /c/Code/Tooling/config/dev/bootstrap-machine.sh --user=$settings["name"]
+Enable-UAC
---
+++ uninstall-onedrive.ps1
@@ -0,0 +1,3 @@
+﻿# Remove OneDrive
+Set-ItemProperty -Path "Registry::HKEY_CLASSES_ROOT\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" -Name "System.IsPinnedToNameSpaceTree" -Value 0
+& "$($env:SystemRoot)\SysWOW64\OneDriveSetup.exe" /uninstall
